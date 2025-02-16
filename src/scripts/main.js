290f6e321d6fba1765c7c37c33992976f9a33f2a
Artem <stilon670@gmail.com>
2025-02-16 10:54:32 +0200

    fix placeholder text

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,14 @@ inputs.forEach((input) => {
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
   label.textContent = input.name;
-  input.placeholder = input.name;
+  input.placeholder = convertCapitalize(input.name);
   parentInput.insertBefore(label, input);
 });
+
+function convertCapitalize(text) {
+  let replaceText = text.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  replaceText = replaceText[0].toUpperCase() + replaceText.slice(1);
+
+  return replaceText;
+}
